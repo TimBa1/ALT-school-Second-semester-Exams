f42f07f328fa524a95f2c926760f9a006a93256d
Timilehin Bakare <timilehinbakare.tb@gmail.com>
2022-11-04 14:27:13 +0100

    new update

---
+++ 2nd-semester-exam/src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Button = ({text, color , handleChange}) => {
+  const  myComponentStyle={
+    backgroundColor: `${color}`,
+    color: 'white',
+    height:"50px",
+    width:"100px",
+    borderRadius:"10px",
+    border:"none",
+    cursor:"pointer",
+    margin:"1rem"
+  }
+
+  return (
+    <button onClick={handleChange} style={ myComponentStyle}>{text}</button>
+  )
+}
+
+export default Button
